b4b960e5139faf9df3f645ba442b7c01efecbcc9
Dave <jaradavid@protonmail.com>
2024-09-27 15:41:14 -0500

    Created basic server to accept connections.

---
+++ src/server/server.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include  "../lib/terrorexchange.h"
+
+int main(int argc, char **argv)
+{
+	// Create connection socket
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(server_fd == -1)
+	{
+		fprintf(stderr, "Failed to create socket.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Bind socket to IP addr and port
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY; // bind to all available network ifaces
+	server_addr.sin_port = htons(8080); // tcp
+
+	if(bind(server_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0)
+	{
+		fprintf(stderr, "Failed to bind socket to TCP port.\n");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+
+	// Listen for connections from clients
+	if(listen(server_fd, 3) < 0)
+	{
+		fprintf(stderr, "Listening failed.\n");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+	printf("Now listening for connections on TCP port 8080.\n");
+
+	// Accept client connections --- just one for now, multithreading/forking/non-blocking IO comes later
+	int addrlen = sizeof(server_addr);
+	int client_socket = accept(server_fd, (struct sockaddr*) &server_addr, (socklen_t*) &addrlen);
+	if(client_socket < 0)
+	{
+		fprintf(stderr, "Failed to accept connection.\n");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+	printf("Client connection accepted.\n");
+
+
+	// Cleanup
+	close(client_socket);
+	close(server_fd);
+	return 0;
+}
