d1f479afb5fa90d2f72c22b0ca09e6893ea09b03
Cordus Bailey <cordusbailey02@gmail.com>
2024-11-07 01:52:13 -0600

    Modified script to compile terroexchangegui

--- src/lib/comp.sh
+++ src/lib/comp.sh
@@ -3,3 +3,6 @@ ar rcs libterrorexchange.a terrorexchange.o
 
 gcc -c secure_connection.c -o secure_connection.o
 ar rcs libsecure_connection.a secure_connection.o
+
+gcc -c terrorexchangegui.c -o terrorexchangegui.o
+ar rcs libterrorexchangegui.a terrorexchangegui.o
