036d98553a16fe64b0156621750780db93c5dace
LaTechBen <bjm056@email.latech.edu>
2024-11-07 16:17:20 -0600

    Added cleanup function to header, fixed name on connection_init

--- src/database/mysql.h
+++ src/database/mysql.h
@@ -3,7 +3,9 @@
 
 enum DATABASE_ERROR { SUCCESS, CONNECTION_ERROR, INEXISTENT_QUERY, INSUFFICIENT_PARAMETERS};
 
-int connection_innit(void);
+void cleanup();
+
+int connection_init(void);
 
 char ***query_mysql(char *query_name, struct payload **inbound_payloads, int *return_flag, int *num_fields, int *num_rows);
 
