e134e863f09c3422f9795e6ed6eacc3ed124efc2
LucasD <lduran6658@gmail.com>
2024-11-08 09:41:58 -0600

    Small Changes
    Both upvote and downvote buttons will disappear when pressed.
    
    When asking a question, now the selected image will show on a label in the window.

--- LinuxPrototypeGUI_v0.9/ui_answerwindow.h
+++ LinuxPrototypeGUI_v0.9/ui_answerwindow.h
@@ -39,15 +39,19 @@ public:
     QPlainTextEdit *plainTextEdit_answerText;
     QHBoxLayout *horizontalLayout;
     QPushButton *pushButton_upAnswer;
+    QVBoxLayout *verticalLayout_4;
+    QLabel *label_4;
     QLabel *label_upAnswerCount;
     QPushButton *pushButton_downAnswer;
+    QVBoxLayout *verticalLayout_5;
+    QLabel *label_5;
     QLabel *label_downAnswerCount;
 
     void setupUi(QDialog *answerWindow)
     {
         if (answerWindow->objectName().isEmpty())
             answerWindow->setObjectName("answerWindow");
-        answerWindow->resize(400, 300);
+        answerWindow->resize(510, 383);
         gridLayout = new QGridLayout(answerWindow);
         gridLayout->setObjectName("gridLayout");
         verticalLayout = new QVBoxLayout();
@@ -153,27 +157,61 @@ public:
         horizontalLayout->setObjectName("horizontalLayout");
         pushButton_upAnswer = new QPushButton(answerWindow);
         pushButton_upAnswer->setObjectName("pushButton_upAnswer");
+        QSizePolicy sizePolicy2(QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Preferred);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(pushButton_upAnswer->sizePolicy().hasHeightForWidth());
+        pushButton_upAnswer->setSizePolicy(sizePolicy2);
 
         horizontalLayout->addWidget(pushButton_upAnswer);
+
+        verticalLayout_4 = new QVBoxLayout();
+        verticalLayout_4->setObjectName("verticalLayout_4");
+        verticalLayout_4->setSizeConstraint(QLayout::SizeConstraint::SetDefaultConstraint);
+        label_4 = new QLabel(answerWindow);
+        label_4->setObjectName("label_4");
+        QFont font1;
+        font1.setPointSize(12);
+        label_4->setFont(font1);
+        label_4->setAlignment(Qt::AlignmentFlag::AlignCenter);
+
+        verticalLayout_4->addWidget(label_4);
 
         label_upAnswerCount = new QLabel(answerWindow);
         label_upAnswerCount->setObjectName("label_upAnswerCount");
         label_upAnswerCount->setFont(font);
         label_upAnswerCount->setAlignment(Qt::AlignmentFlag::AlignCenter);
 
-        horizontalLayout->addWidget(label_upAnswerCount);
+        verticalLayout_4->addWidget(label_upAnswerCount);
+
+
+        horizontalLayout->addLayout(verticalLayout_4);
 
         pushButton_downAnswer = new QPushButton(answerWindow);
         pushButton_downAnswer->setObjectName("pushButton_downAnswer");
+        sizePolicy2.setHeightForWidth(pushButton_downAnswer->sizePolicy().hasHeightForWidth());
+        pushButton_downAnswer->setSizePolicy(sizePolicy2);
 
         horizontalLayout->addWidget(pushButton_downAnswer);
+
+        verticalLayout_5 = new QVBoxLayout();
+        verticalLayout_5->setObjectName("verticalLayout_5");
+        label_5 = new QLabel(answerWindow);
+        label_5->setObjectName("label_5");
+        label_5->setFont(font1);
+        label_5->setAlignment(Qt::AlignmentFlag::AlignCenter);
+
+        verticalLayout_5->addWidget(label_5);
 
         label_downAnswerCount = new QLabel(answerWindow);
         label_downAnswerCount->setObjectName("label_downAnswerCount");
         label_downAnswerCount->setFont(font);
         label_downAnswerCount->setAlignment(Qt::AlignmentFlag::AlignCenter);
 
-        horizontalLayout->addWidget(label_downAnswerCount);
+        verticalLayout_5->addWidget(label_downAnswerCount);
+
+
+        horizontalLayout->addLayout(verticalLayout_5);
 
 
         verticalLayout_2->addLayout(horizontalLayout);
@@ -209,8 +247,10 @@ public:
         label_2->setText(QCoreApplication::translate("answerWindow", "Answer", nullptr));
         plainTextEdit_answerText->setPlainText(QCoreApplication::translate("answerWindow", "Read only too", nullptr));
         pushButton_upAnswer->setText(QString());
+        label_4->setText(QCoreApplication::translate("answerWindow", "Upvotes", nullptr));
         label_upAnswerCount->setText(QCoreApplication::translate("answerWindow", "0", nullptr));
         pushButton_downAnswer->setText(QString());
+        label_5->setText(QCoreApplication::translate("answerWindow", "Downvotes", nullptr));
         label_downAnswerCount->setText(QCoreApplication::translate("answerWindow", "0", nullptr));
     } // retranslateUi
 
